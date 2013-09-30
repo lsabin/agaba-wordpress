bf0ca833ebc32c72c903086cbb3e937b5da86c86
lsabin <sabin.leandro@gmail.com>
2013-09-30 11:30:38 +0200

    pagina de revista grolos

--- header-agaba.php
+++ header-agaba.php
@@ -7,7 +7,7 @@
     <title><?php wp_title('|', true, 'right'); ?> <?php bloginfo('name'); ?> <?php if ( !wp_title('', true, 'left') ); { ?> | <?php bloginfo('description'); ?> <?php } ?></title>
     
 
-    <link href='http://fonts.googleapis.com/css?family=Libre+Baskerville:400,400italic|Lobster+Two:400italic,700italic' 
+    <link href='http://fonts.googleapis.com/css?family=Libre+Baskerville:400,400italic|Lobster+Two:400italic,700italic|Voltaire' 
       rel='stylesheet' type='text/css'>    
     <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'template_url' ); ?>/css/bootstrap.css" />
     <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'template_url' ); ?>/css/style-black.css" />
